4981a883f7205ea8fa60ad17738e95b9c42f5f7a
adam-poulton <adam.poulton5@gmail.com>
2020-01-11 00:07:32 +1100

    Add references to game.rb & player.rb

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,3 +1,15 @@
 require_relative "./cell.rb"
 require_relative "./board.rb"
 require_relative "./core_extensions.rb"
+require_relative "./game.rb"
+require_relative "./player.rb"
+
+include ConnectFour
+
+puts "Welcome to Connect Four!"
+puts ""
+puts "Enter player 1:"
+p1 = Player.new({color: "X", name: gets.chomp})
+puts "Enter player 2:"
+p2 = Player.new({color: "O", name: gets.chomp})
+Game.new([p1, p2]).play
